9c71dae19efd760b1854e5f0e85851ed8fe4aa96
ladeynn <85115286+ladeynn@users.noreply.github.com>
2024-11-07 12:05:35 +0000

    Fleeing Part 2

---
+++ Assets/Scripts/AIControl.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    NavMeshAgent agent;
+    Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
+
+    void Start() {
+
+        agent = GetComponent<NavMeshAgent>();
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        ResetAgent();
+    }
+
+    void ResetAgent()
+    {
+        float speedMult = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMut", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10.0f;
+                agent.angularSpeed = 500;
+            }
+        }
+    }
+
+    void Update() {
+        if (agent.remainingDistance < 1) 
+        {
+            ResetAgent();
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
+    }
+}
